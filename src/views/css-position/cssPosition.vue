3a121d60ecaf58b5d379ec34fbe8f855c2bf2ab2
412845222 <412845222@qq.com>
2022-03-14 20:51:22 +0800

    master

--- src/views/css-position/cssPosition.vue
+++ src/views/css-position/cssPosition.vue
@@ -1,23 +1,8 @@
-
+<!-- <script setup lang="ts" src="./cssPosition.ts"></script> -->
 <template>
-  <div id="css-position">
-    <Scene>
-      <Player></Player>
-    </Scene>
-  </div>
+  <div id="css-position"></div>
 </template>
 
-<style scoped>
-#css-position {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+<style scoped></style>
 
-<script setup lang="ts">
-import Scene from './components/Scene.vue'
-import Player from './components/Player.vue'
-</script>
+<script setup lang="ts"></script>

8d3f9aa8f0bd4d782a1364365da992fca77e3617
412845222 <412845222@qq.com>
2022-03-14 21:56:09 +0800

    master

---
+++ src/views/css-position/components/Scene.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="scene">
+    <slot></slot>
+    <img class="scene_img" src="../../../assets/img/background.png" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.scene {
+  width: 960px;
+  height: 540px;
+  border: 1px solid #fff;
+  overflow: hidden;
+  position: relative;
+}
+.scene_img {
+  height: 100%;
+}
+</style>
--- src/views/css-position/cssPosition.vue
+++ src/views/css-position/cssPosition.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="css-position"></div>
+  <div id="css-position">
+    <Scene>
+      <Player></Player>
+    </Scene>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#css-position {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Scene from "./components/Scene.vue";
+import Player from "./components/Player.vue";
+</script>
